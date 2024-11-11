737aa9737728743d6b4e11b4e53b1bfee5b4b64f
shoab ahmed <adam_ahmed6@hotmail.com>
2024-11-11 14:35:57 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PortfolioOptimizer from './PortfolioOptimizer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PortfolioOptimizer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
